46c839ea8f7b743d117946bfbce40053b7ac1fe9
Rachel <149018958+rachellin94@users.noreply.github.com>
2023-12-21 18:53:58 -0800

    Update 12_21_2023.py

--- 12_21_2023.py
+++ 12_21_2023.py
@@ -7,3 +7,12 @@
 def next_edge(side1, side2):
     max_edge = side1 + side2 - 1
     return(max_edge)
+
+# Create a function that takes two numbers as arguments and returns their sum.
+# addition(3, 2) ➞ 5
+# addition(-3, -6) ➞ -9
+# addition(7, 3) ➞ 10
+
+def addition(a, b):
+    sum_up = a + b
+    return(sum_up)
