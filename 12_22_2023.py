a41da709a3c1cea5c6e7b0fe2028346b531d4664
Rachel <149018958+rachellin94@users.noreply.github.com>
2023-12-22 10:41:42 -0800

    Update 12_22_2023.py

--- 12_22_2023.py
+++ 12_22_2023.py
@@ -1,1 +1,9 @@
+# Return the Next Number from the Integer Passed
+# Examples
+# addition(0) ➞ 1
+# addition(9) ➞ 10
+# addition(-3) ➞ -2
 
+def addition(num):
+    new_number = num + 1 
+    return new_number
