5353815efaca7a243a272aa2495f920e46651923
Rachel <149018958+rachellin94@users.noreply.github.com>
2023-12-23 18:46:16 -0800

    Update 12_23_2023.py

--- 12_23_2023.py
+++ 12_23_2023.py
@@ -3,3 +3,15 @@ def convert(minutes):
 	mins_to_seconds = minutes * 60
 	return mins_to_seconds
   
+# Find the Discount
+# Create a function that takes two arguments: the original price and the discount percentage as integers and returns the final price after the discount.
+# Examples
+# dis(1500, 50) ➞ 750
+
+def dis(price, discount):
+    price = int(price)
+    discount = float(discount / 100)
+    final_discount = 1 - discount 
+    total_price = price * final_discount
+    round_price = round(total_price, 2)
+    return round_price 
