ebd8417e82cbae1c4a1b2baee3e6b54603633963
Rachel <149018958+rachellin94@users.noreply.github.com>
2023-12-28 09:32:01 -0800

    Update 12_28_2023.py

--- 12_28_2023.py
+++ 12_28_2023.py
@@ -1,1 +1,18 @@
+def is_armstrong_number(num):
+    original_num = num  # Save the original number for comparison later
+    i = 0
+    while num > 0:
+        num //= 10
+        i += 1
 
+    return required_sum(original_num, i) == original_num
+
+def required_sum(num, i):
+    s = 0
+    
+    while num > 0:
+        digit = num % 10
+        num //= 10
+        s += pow(digit, i)
+        
+    return s
