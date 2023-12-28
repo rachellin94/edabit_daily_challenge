82f724ec38877a1630d6fae399ce0f76202c4fc6
Rachel <149018958+rachellin94@users.noreply.github.com>
2023-12-27 22:47:55 -0800

    Update 12_27_2023.py

--- 12_27_2023.py
+++ 12_27_2023.py
@@ -1,1 +1,12 @@
+# Grains 
 
+def square(number):
+    if number < 1 or number > 64:
+        raise ValueError("square must be between 1 and 64")
+
+    grains_on_square = 2 ** (number - 1)
+    return grains_on_square
+
+def total():
+    grains_on_chessboard = 2 ** 64 - 1
+    return grains_on_chessboard
